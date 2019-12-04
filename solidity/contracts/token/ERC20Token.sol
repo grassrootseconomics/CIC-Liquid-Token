5cf00451da0cc51db1d4b76d7e8b482d085f9423
Nick Williams <enjeyw@gmail.com>
2019-12-04 15:38:53 +1100

    disabling requirement for allowance to be 0 before changing as it causes big bottlenecks. Not a large security risk given that requirement isn't enforced by Open Zeppelin

--- solidity/contracts/token/ERC20Token.sol
+++ solidity/contracts/token/ERC20Token.sol
@@ -113,7 +113,7 @@ contract ERC20Token is IERC20Token, Utils {
         returns (bool success)
     {
         // if the allowance isn't 0, it can only be updated to 0 to prevent an allowance change immediately after withdrawal
-        require(_value == 0 || allowance[msg.sender][_spender] == 0, 'Value or current allowance is not 0');
+//        require(_value == 0 || allowance[msg.sender][_spender] == 0, 'Value or current allowance is not 0');
 
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
